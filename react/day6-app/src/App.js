b7b81ac3320332fe4eca055431c1a233ae5ca73a
mansi malunjkar <mansimalunjkar2018@gmail.com>
2021-07-30 10:54:16 +0530

    new branch

--- react/day6-app/src/App.js
+++ react/day6-app/src/App.js
@@ -4,18 +4,36 @@ import { useState } from 'react';
 
 function App() {
 
-let [name]=useState("cdac");//string variable
-let [counter]=useState(100);//number variable
-let [active]=useState("true");//boolean
-let [list] = useState(["delhi"]); //array of list
-let [std1] = useState({name:"mansi"});//object literals
+// let [name]=useState("cdac");//string variable
+// let [counter]=useState(100);//number variable
+// let [active]=useState("true");//boolean
+// let [list] = useState(["delhi"]); //array of list
+// let [std1] = useState({name:"mansi"});//object literals
 
+let [comment,setComment]=useState("");
+let [comment2,setComment2]=useState([]);
+let Post=()=>
+{
+  let add = [comment,...comment2];
+  setComment2(add);
+  console.log(add);
+};
+let handleInputChange=(e)=>{
+  
+  console.log(e.target.value);
+  let finalcomment=e.target.value;
+  setComment(finalcomment);
+}
   return (
+    // <div>
+    //       <h1 className="bg-primary p-3">learning to declare stateful variable</h1>
+    //       <h1>String :: {name}</h1>
+    // </div>
     <div>
-          <h1 className="bg-primary p-3">learning to declare stateful variable</h1>
-          <h1>String :: {name}</h1>
+    <input type="text" placeholder="post here" value={comment} onChange={handleInputChange}></input>
+    <button type="button" onClick={Post}>Post</button>
+    <div>{comment2.map((item)=>{return <h1>{item}</h1>;})}</div>
     </div>
-
   );
 }
 
