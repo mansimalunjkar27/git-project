2382f8d2cbe863f326fd5170db44c31ee982367d
mansi malunjkar <mansimalunjkar2018@gmail.com>
2021-07-30 17:34:48 +0530

    stateful example

--- react/day6-app/src/App.js
+++ react/day6-app/src/App.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 function App() 
 {
-  //stateless member
-  let counter=100;
-
-  let increment=()=>{
-    counter += 1;
-  console.log(counter);
-  };
-  
+ let[name] = useState("cdac");
+ let[counter] = useState(100);
+ let[active] = useState(false);
+ let[list] = useState(["delhi","pune","mumbai"]);
+ let[std1] = useState({name:"mansi"});
 
   return (
     <div>
-      <h1>Stateless Does not Update UI</h1>
-      {counter}
-      <input type="button" value="Increment" onClick={increment}></input>
+      <h1>learning to declare stateful variable</h1>
+      <h1>String :: {name}</h1>
+      <h1>Number :: {counter}</h1>
+      <h1>Boolean :: {active + ""}</h1>
+      <h1>List :: {list}</h1>
+      <h1>Object :: {JSON.stringify(std1)}</h1>
+      <h1>Objec :: {std1.name} </h1>
     </div>
   );
 }
