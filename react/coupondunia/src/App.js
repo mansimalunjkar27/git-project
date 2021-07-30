5a627799baf541708b6d1f0118043dd6dcba6924
mansi malunjkar <mansimalunjkar2018@gmail.com>
2021-07-31 04:17:11 +0530

    commit new change

--- react/coupondunia/src/App.js
+++ react/coupondunia/src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./components/Pages/Home";
+import { About } from "./components/Pages/About";
+import { Blog } from "./components/Pages/Blog";
+import { Contact } from "./components/Pages/Contact";
+import { Register } from "./components/Pages/Register";
 
 function App() {
   return (
-    <div className="App">
-     hello
-     </div>
+    <>
+      <Router>
+        <NavBar />
+
+        <div className="pages">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/About" component={About} />
+            <Route path="/Blog" component={Blog} />
+            <Route path="/Contact" component={Contact} />
+            <Route path="/Register" component={Register} />
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
